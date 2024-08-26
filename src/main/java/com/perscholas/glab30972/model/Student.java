0dbe99b2399fa887625ec46982f62ac54f11ade9
Marlie Martinez <marlieahue@gmail.com>
2024-08-26 15:02:34 -0400

    first commit

---
+++ src/main/java/com/perscholas/glab30972/model/Student.java
@@ -0,0 +1,21 @@
+package com.perscholas.glab30972.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@AllArgsConstructor
+@Data
+@Entity
+@NoArgsConstructor
+public class Student {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    @Column(unique = true)
+    private Long accountNo;
+    private Long marks;
+}
+
